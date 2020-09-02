48f400e1bf32dfb3590aba7b6da5ae551dd00631
Mona Lisa <email@example.com>
2020-09-02 12:45:04 -0600

    starting

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace BankEncapsulation
+{
+    //Create a new Console Application in your repos folder
+    //name it BankEncapsulation
+
+    //Create a new public class named BankAccount
+
+    public class BankAccount
+    {
+        
+        public BankAccount()
+        {
+
+        }
+
+     //A private field of type double named balance with a value of 0
+
+        private double balance = 0;
+
+     //Define a method named Deposit that will accept a double and store that
+     // value in the balance field
+
+        public void Deposit()
+        {
+            Console.WriteLine($"Enter amount of your deposit: $ ");
+            var depositAmount = double.Parse(Console.ReadLine());
+            balance = depositAmount;
+        }
+
+        // Define a method named GetBalance that will return
+        //the amount stored in the balance field
+
+        public double GetBalance()
+        {
+            Console.WriteLine($" ${balance} has been deposited.");
+            return balance;
+            
+        }
+
+    }
+}
+  
---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //In the main method of your application, create a new instance of
+            //the BankAccount class.  Allow the user of the application to
+            //Deposit money and retrieve their balance through the console.
+
+            var yourAccount = new BankAccount();
+
+            yourAccount.Deposit();
+
+            yourAccount.GetBalance();
+
+        }
+    }
+}
